ec75b5e12f0d537107c399611d7a74d407c41820
DigsRodrigues <diegohrodrigues.993@gmail.com>
2024-07-10 23:18:25 -0300

    corrigindo erro

--- Logica_de_programacao/Exercicios/Ex01/ex01.h
+++
@@ -1,7 +0,0 @@
-/**
- * @brief Calculo do desconto com relação a idade.
- * @param valCompra Valor da compra sem desconto.
- * @param idade Idade do comprador.
- * @return Novo valor a ser comprado, ja com o desconto aplicado.
- */
-float desconto(float valCompra, int idade);
--- Logica_de_programacao/Exercicios/Ex04/README.mk
+++
@@ -1,4 +0,0 @@
-Exercício 4 - Determinando o período do dia
-
-Escreva um programa que leia a hora atual (um número de 0 a 23) e determine se é manha (5 a 11), tarde(12 a 17), noite (18 a 22) ou madrugada (23 a 4).
-Usando o operador ternário ?:..
